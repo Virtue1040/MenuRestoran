b16f72f9190d4f802155498dfe1514b4d51f7f09
rafi.hidayat1414 <rafi.hidayat1414@gmail.com>
2025-06-13 13:06:36 +0000

    fix primary key

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,8 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $primaryKey = "id_user";
+
     /**
      * The attributes that should be hidden for serialization.
      *
