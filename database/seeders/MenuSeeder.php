2f8d7cb0615b8ca7407c499b2011ee3ece851573
rafi.hidayat1414 <rafi.hidayat1414@gmail.com>
2025-06-13 15:10:24 +0000

    update seeder

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Menu;
 
 class MenuSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class MenuSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Menu::create([
+            'judul' => 'Sate Ayam',
+            'kategori' => 'Makanan',
+            'asal' => 'Madura',
+            'image' => 'upload/sate.jpg',
+        ]);
+
+        Menu::create([
+            'judul' => 'Es Cendol',
+            'kategori' => 'Minuman',
+            'asal' => 'Sunda',
+            'image' => 'upload/cendol.jpg',
+        ]);
     }
 }
